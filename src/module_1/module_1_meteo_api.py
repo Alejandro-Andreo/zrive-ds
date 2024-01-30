b9f0218b28dfd969fa8ab153d30dd42559e02549
Alejandro <100408869@alumnos.uc3m.es>
2024-01-30 03:30:26 +0100

    Modulo 1 terminado

--- src/module_1/module_1_meteo_api.py
+++ src/module_1/module_1_meteo_api.py
@@ -1,19 +1,21 @@
-import pandas as pd 
-import requests 
+import pandas as pd
+import numpy as np
+import requests
 import matplotlib.pyplot as plt
 import time
 import logging
 from requests.models import Response
-from datetime import datetime
+
 
 API_URL = "https://climate-api.open-meteo.com/v1/climate?"
 COORDINATES = {
-"Madrid": {"latitude": 40.416775, "longitude": -3.703790},
-"London": {"latitude": 51.507351, "longitude": -0.127758},
-"Rio": {"latitude": -22.906847, "longitude": -43.172896},
+    "Madrid": {"latitude": 40.416775, "longitude": -3.703790},
+    "London": {"latitude": 51.507351, "longitude": -0.127758},
+    "Rio": {"latitude": -22.906847, "longitude": -43.172896},
 }
-VARIABLES = ["temperature_2m_mean","precipitation_sum","soil_moisture_0_to_10cm_mean"]
-MODELS = ["CMCC_CM2_VHR4",
+VARIABLES = ["temperature_2m_mean", "precipitation_sum", "soil_moisture_0_to_10cm_mean"]
+MODELS = [
+    "CMCC_CM2_VHR4",
     "FGOALS_f3_H",
     "HiRAM_SIT_HR",
     "MRI_AGCM3_2_S",
@@ -24,64 +26,82 @@ MODELS = ["CMCC_CM2_VHR4",
 MAX_CALL_ATTEMPTS = 50
 
 # Logging configuration
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
 
 def validate_response(response: Response) -> bool:
-    # Check if the response is a dictionary
     if not isinstance(response, dict):
         return False
 
     # Check if the required keys are in the response
-    required_keys = ['latitude', 'longitude', 'generationtime_ms', 'timezone', 'timezone_abbreviation', 'daily', 'daily_units']
+    required_keys = [
+        "latitude",
+        "longitude",
+        "generationtime_ms",
+        "timezone",
+        "timezone_abbreviation",
+        "daily",
+        "daily_units",
+    ]
     if not all(key in response for key in required_keys):
         return False
 
     # Check if 'daily' is a dictionary
-    if not isinstance(response['daily'], dict):
+    if not isinstance(response["daily"], dict):
         return False
 
     # Check if 'daily_units' is a dictionary
-    if not isinstance(response['daily_units'], dict):
+    if not isinstance(response["daily_units"], dict):
         return False
 
     return True
 
-def call_api (params: dict):
+
+def call_api(params: dict):
     """
     Makes an API call with given parameters, handling rate limits and other errors.
     :param params: Dictionary with the parameters to be passed to the API call.
     :return: The API response if successful, None otherwise.
     """
-    call_attempts= 0 
-    backoff_time = 60 
+    call_attempts = 0
+    backoff_time = 60
     while call_attempts < MAX_CALL_ATTEMPTS:
         try:
             response = requests.get(API_URL, params=params)
             if response.status_code == 200:
-                if validate_response(response):
+                if validate_response(response.json()):
                     return response
             elif response.status_code == 429:
                 # Use the Retry-After header to determine the cool-off time
-                cool_off_time = response.headers.get("Retry-After",backoff_time)
-                logging.debug(f"Rate limit exceeded. Waiting for {cool_off_time} seconds.")
-                logging.debug(response.text)
+                cool_off_time = response.headers.get("Retry-After", backoff_time)
+                logging.info(
+                    f"Rate limit exceeded. Waiting for {cool_off_time} seconds."
+                )
+                logging.info(response.text)
                 time.sleep(cool_off_time)
                 backoff_time *= 2
-            else: 
-                raise Exception(f"API request failed with status code {response.status_code}")
+            else:
+                raise Exception(
+                    f"API request failed with status code {response.status_code}"
+                )
         except Exception as e:
-            logging.debug(f"API request failed with exception {e}")
+            logging.info(f"API request failed with exception {e}")
             break
         call_attempts += 1
-        logging.debug(f"Call attempts: {call_attempts}") 
-    logging.debug(f"Max number of call attempts reached. Returning empty result")
+        logging.info(f"Call attempts: {call_attempts}")
+    logging.info("Max number of call attempts reached. Returning empty result")
     return response
- 
-        
-def get_data_meteo_api(city: str, start_date: str, end_date: str, variable: str, model:str) -> pd.DataFrame():
+
+
+def get_data_meteo_api(
+    city: str, start_date: str, end_date: str, variable: str, model: str
+) -> pd.DataFrame():
     """
     Fetches daily weather data for a specific city and variable from the Meteo API.
-    :param city: The name of the city to fetch the data for. Must be a key in the COORDINATES dictionary.
+    :param city: The name of the city to fetch the data for. Must be a key in the \
+    COORDINATES dictionary.
     :param start_date: The start date for the data in the format 'YYYY-MM-DD'.
     :param end_date: The end date for the data in the format 'YYYY-MM-DD'.
     :param variable: The weather variable to fetch the data for.
@@ -94,9 +114,9 @@ def get_data_meteo_api(city: str, start_date: str, end_date: str, variable: str,
         "latitude": COORDINATES[city]["latitude"],
         "longitude": COORDINATES[city]["longitude"],
         "start_date": start_date,
-        "end_date": end_date,  
-        "models":  model,
-        "daily": variable
+        "end_date": end_date,
+        "models": model,
+        "daily": variable,
     }
     data = call_api(params)
     if data is None:
@@ -105,81 +125,124 @@ def get_data_meteo_api(city: str, start_date: str, end_date: str, variable: str,
     # Convert the daily data into a DataFrame
     df = pd.DataFrame(daily_data)
     # Add the city name as a column
-    df['city'] = city
-    df['time'] = pd.to_datetime(df['time'])
+    df["city"] = city
+    df["time"] = pd.to_datetime(df["time"])
     return df
+
 
 def calculate_mean_std(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Calculates the mean and standard deviation for each prefix in the DataFrame's columns.
+    Calculates the mean and standard deviation for each prefix in the \
+    DataFrame's columns.
     :param df: The DataFrame to calculate the mean and standard deviation for.
-    :return: A new DataFrame with the mean and standard deviation for each prefix in the original DataFrame's columns.
+    :return: A new DataFrame with the mean and standard deviation for each prefix in the 
+    original DataFrame's columns.
     """
     df = df.copy()
     # Extracting prefixes (assuming they are separated by underscores)
-    prefixes = set(col.split('_')[0] for col in df.columns if '_' in col)
+    prefixes = set(col.split("_")[0] for col in df.columns if "_" in col)
     for prefix in prefixes:
         relevant_cols = [col for col in df.columns if col.startswith(prefix)]
         mean_values = df[relevant_cols].mean(axis=1)
         df[prefix + "_mean"] = mean_values
         std_values = df[relevant_cols].std(axis=1)
-        df[prefix + '_std'] = std_values
+        df[prefix + "_std"] = std_values
         df = df.drop(relevant_cols, axis=1)
     return df
 
-def plot_data (df: pd.DataFrame, variable: str) -> None:
-    """
-    Plots the annual mean and dispersion of a specified variable for each city in the DataFrame.
 
+def plot_data(df: pd.DataFrame, variable: str) -> None:
+    """
+    Plots the annual mean and dispersion of a specified variable for each city in \
+    the DataFrame.
     :param df: The DataFrame containing the data to plot.
     :param variable: The variable to plot.
     :return: None
     """
-    plt.figure(figsize=(10, 6))
-    
-    for city in COORDINATES.keys():
-        city_data = df[df['city'] == city]
-        plt.errorbar(city_data['year'], city_data[f'{variable}_mean'], 
-                     yerr=city_data[f'{variable}_std'], label=city, fmt='-o', capsize=5)
-    
-    plt.title(f'Annual Mean and Dispersion of {variable.capitalize()}')
-    plt.xlabel('Year')
-    plt.ylabel(f'{variable.capitalize()}')
-    plt.legend()
+
+    # Create a mapping from short variable names to full names
+    short_to_full = {v.split("_")[0]: v for v in VARIABLES}
+
+    # Retrieve the full name of the variable
+    full_variable_name = short_to_full.get(variable, variable)
+
+    plt.figure(figsize=(12, 8))
+
+    # Create a distinct color for each city
+    colors = plt.cm.viridis(np.linspace(0, 1, len(COORDINATES)))
+
+    for idx, city in enumerate(COORDINATES.keys()):
+        city_data = df[df["city"] == city]
+        mean = city_data[f"{variable}_mean"]
+        std = city_data[f"{variable}_std"]
+        plt.plot(
+            city_data["year"],
+            mean,
+            label=city,
+            color=colors[idx],
+            marker="o",
+            linestyle="-",
+            linewidth=2,
+        )
+        plt.fill_between(
+            city_data["year"], mean - std, mean + std, color=colors[idx], alpha=0.3
+        )
+
+    plt.title(
+        f'Annual Mean and Dispersion of {full_variable_name.replace("_", " ").capitalize()}',
+        fontsize=14,
+    )
+    plt.xlabel("Year", fontsize=12)
+    plt.ylabel(full_variable_name.replace("_", " ").capitalize(), fontsize=12)
+    plt.xticks(fontsize=10)
+    plt.yticks(fontsize=10)
+    plt.grid(True)
+    plt.legend(loc="upper left", bbox_to_anchor=(1, 1), fontsize=10)
+    plt.tight_layout()
     plt.show()
-    
+
+
 def main():
     list_data_cities = []
     for city in COORDINATES.keys():
         logging.info(f"Getting data for {city}")
-        list_data_variables= []
+        list_data_variables = []
         for variable in VARIABLES:
             logging.info(f"Getting data for {variable} in {city}")
             for model in MODELS:
                 logging.info(f"Getting data for {model} in {city}")
-                data = get_data_meteo_api(city, "1950-01-01", "1960-12-31", variable, model)
-                logging.debug(data.head())
+                data = get_data_meteo_api(
+                    city, "1950-01-01", "1960-12-31", variable, model
+                )
+                logging.info(data.head())
                 list_data_variables.append(data)
         combined_df = list_data_variables[0]
-        for df in list_data_variables[1:]:
-            combined_df = pd.merge(combined_df,df, on=['time', 'city'])
-        
+        for i, df in enumerate(list_data_variables[1:], 1):
+            combined_df = pd.merge(
+                combined_df, df, on=["time", "city"], suffixes=("", f"_df{i}")
+            )
+
         data_with_calculation = calculate_mean_std(combined_df)
         list_data_cities.append(data_with_calculation)
-              
+
     df_total = pd.concat(list_data_cities)
-    
-    df_total['time'] = pd.to_datetime(df_total['time'])
-    df_total['year'] = df_total['time'].dt.year
-    
-    # Annualize the data by taking the mean of the daily values for a given year
-    annual_data = df_total.groupby(['city', 'year']).mean().reset_index()
-    
+
+    df_total["time"] = pd.to_datetime(df_total["time"])
+    df_total["year"] = df_total["time"].dt.year
+
+    annual_data = df_total.groupby(["city", "year"]).mean().reset_index()
+    annual_data.to_csv("src/module_1/annual_data.csv", index=False)
+
+    annual_data = pd.read_csv("src/module_1/annual_data.csv")
+    annual_data = annual_data.drop(["time"], axis=1)
+
     # Plot the data for each variable dynamically
-    for variable in annual_data.columns:
-        if variable not in ['city', 'year']:
-            plot_data(annual_data, variable)
-    
+    variables = set(
+        col.split("_")[0] for col in annual_data.columns if col not in ["city", "year"]
+    )
+    for variable in variables:
+        plot_data(annual_data, variable)
+
 
 if __name__ == "__main__":
     main()
--- tests/module_1/test_meteo_api.py
+++ tests/module_1/test_meteo_api.py
@@ -1,10 +1,14 @@
 import unittest
 from unittest.mock import patch, Mock
-import pandas as pd 
-from src.module_1.module_1_meteo_api import main, validate_response, get_data_meteo_api, calculate_mean_std, call_api
+import pandas as pd
+from src.module_1.module_1_meteo_api import (
+    validate_response,
+    calculate_mean_std,
+    call_api,
+)
+
 
 class TestValidateResponse(unittest.TestCase):
-
     def test_validate_response_valid(self):
         response = {
             "latitude": 40.416775,
@@ -13,7 +17,7 @@ class TestValidateResponse(unittest.TestCase):
             "timezone": "Europe/Madrid",
             "timezone_abbreviation": "CEST",
             "daily": {},
-            "daily_units": {}
+            "daily_units": {},
         }
         self.assertTrue(validate_response(response))
 
@@ -23,24 +27,23 @@ class TestValidateResponse(unittest.TestCase):
 
 
 class TestCalculateMeanStd(unittest.TestCase):
-
     def test_calculate_mean_std(self):
         data = {
-            'time': ['2022-01-01', '2022-01-02'],
-            'temperature_2m_AAAAAAAA': [10, 20],
-            'temperature_2m_BBBBBBBB': [15, 25],
-            'city': ['City1', 'City1']
+            "time": ["2022-01-01", "2022-01-02"],
+            "temperature_2m_AAAAAAAA": [10, 20],
+            "temperature_2m_BBBBBBBB": [15, 25],
+            "city": ["City1", "City1"],
         }
         df = pd.DataFrame(data)
         result = calculate_mean_std(df)
-        self.assertIn('temperature_mean', result.columns)
-        self.assertIn('temperature_std', result.columns)
-        self.assertEqual(result['temperature_mean'][0], 12.5)
-        self.assertEqual(result['temperature_mean'][1], 22.5)
+        self.assertIn("temperature_mean", result.columns)
+        self.assertIn("temperature_std", result.columns)
+        self.assertEqual(result["temperature_mean"][0], 12.5)
+        self.assertEqual(result["temperature_mean"][1], 22.5)
+
 
 class TestCallAPI(unittest.TestCase):
-
-    @patch('src.module_1.module_1_meteo_api.requests.get')
+    @patch("src.module_1.module_1_meteo_api.requests.get")
     def test_call_api_successful(self, mock_get):
         # Mock a successful API response
         mock_response = Mock()
@@ -55,7 +58,7 @@ class TestCallAPI(unittest.TestCase):
         result = call_api(params)
         self.assertEqual(result.status_code, 200)
 
-    @patch('src.module_1.module_1_meteo_api.requests.get')
+    @patch("src.module_1.module_1_meteo_api.requests.get")
     def test_call_api_failure(self, mock_get):
         # Mock a failed API response
         mock_response = Mock()
@@ -66,11 +69,9 @@ class TestCallAPI(unittest.TestCase):
         self.assertEqual(result.status_code, 404)
 
 
-
-
-
 def test_main():
     raise NotImplementedError
+
 
 if __name__ == "__main__":
     unittest.main()
